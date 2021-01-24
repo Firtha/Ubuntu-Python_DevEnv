559fe9cf5f913f99d261cfccd01dde36727ddfa6
Firtha <svensson.jeremy94@gmail.com>
2021-01-24 15:19:27 +0100

    Edit provisionning for pip3 install

--- provisionning.sh
+++ provisionning.sh
@@ -5,10 +5,17 @@ sudo apt-get -y upgrade
 
 # Python
 sudo apt-get install python3.6
-sudo apt-get install python-pip -y
+sudo apt-get install python3-pip -y
+
+# Import program
+cp /vagrant/Cryptography-TP.py . 
 
 ## Test python with :
 # python3 --version
+# pip3 --version
+
+# pip3 install bitcoin
+# python3.6 Cryptography-TP.py
 
 echo "************"
 echo "INSTALL DONE"
