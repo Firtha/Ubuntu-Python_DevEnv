b7f68f6d879d43f53b2c9bdc18929edc3473941a
Firtha <svensson.jeremy94@gmail.com>
2021-01-26 11:04:54 +0100

    Separate KeyDerivations and Encryption/Decryption

--- provisionning.sh
+++ provisionning.sh
@@ -7,15 +7,20 @@ sudo apt-get -y upgrade
 sudo apt-get install python3.6
 sudo apt-get install python3-pip -y
 
-# Import program
-cp /vagrant/Cryptography-TP.py . 
+# Import programs
+cp /vagrant/KeyDerivations.py .
+cp /vagrant/EncryptDecrypt.py .
 
 ## Test python with :
 # python3 --version
 # pip3 --version
 
+## Install Python moduls :
 # pip3 install bitcoin
-# python3.6 Cryptography-TP.py
+
+# pip3 install bitcoin
+# python3.6 KeyDerivations.py
+# python3.6 EncryptDecrypt.py
 
 echo "************"
 echo "INSTALL DONE"
