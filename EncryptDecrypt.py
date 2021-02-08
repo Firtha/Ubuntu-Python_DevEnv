517ce7d7d029ac8e0559c09b14911d6712ca8fb4
Firtha <svensson.jeremy94@gmail.com>
2021-02-08 16:03:31 +0100

    Added directories management

--- EncryptDecrypt.py
+++ EncryptDecrypt.py
@@ -1,6 +1,9 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 import binascii
+from os import listdir
+from os.path import isfile, join
+import os.path
 
 keyPair = RSA.generate(3072)
 
@@ -13,12 +16,41 @@ print(f"\n\nPrivate key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
 privKeyPEM = keyPair.exportKey()
 print(privKeyPEM.decode('ascii'))
 
-msg = b'A message for encryption'
-print("\n\nBefore encryption: ", msg)
-encryptor = PKCS1_OAEP.new(pubKey)
-encrypted = encryptor.encrypt(msg)
-print("\nEncrypted: ", binascii.hexlify(encrypted))
 
-decryptor = PKCS1_OAEP.new(keyPair)
-decrypted = decryptor.decrypt(encrypted)
-print('\nDecrypted: ', decrypted)
+onlyfiles = [f for f in listdir("files-to-encrypt") if isfile(join("files-to-encrypt", f))]
+
+for f in onlyfiles:
+    print("\n\nFile is: ", f)
+    reader = open("files-to-encrypt/" + f, "rb")
+    msg = reader.read()
+    print("\nBefore encryption: ", msg)
+    encryptor = PKCS1_OAEP.new(pubKey)
+    encrypted = encryptor.encrypt(msg)
+    print("\nEncrypted: ", binascii.hexlify(encrypted))
+    reader.close()
+
+    if not os.path.exists("files-encrypted/"):
+        os.mkdir("files-encrypted/")
+        print("Directory " , "files-encrypted/",  " Created ")
+    else:    
+        print("Directory " , "files-encrypted/",  " already exists")
+
+    filename = f.split(".")
+    writer = open("files-encrypted/" + filename[0] + "_crypted.txt", "wb")
+    writer.write(encrypted)
+    writer.close()
+
+    decryptor = PKCS1_OAEP.new(keyPair)
+    decrypted = decryptor.decrypt(encrypted)
+    print('\nDecrypted: ', decrypted)
+
+for f in onlyfiles:
+    print("\n\nFile is: ", f)
+    filename = f.split(".")
+    reader = open("files-encrypted/" + filename[0] + "_crypted.txt", "rb")
+    encrypted = reader.read()
+    print("\nBefore decryption: ", binascii.hexlify(encrypted))
+    decryptor = PKCS1_OAEP.new(keyPair)
+    decrypted = decryptor.decrypt(encrypted)
+    print('\nDecrypted: ', decrypted)
+    reader.close()
